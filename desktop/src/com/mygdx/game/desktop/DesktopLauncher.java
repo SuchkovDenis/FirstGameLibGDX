6181e4bc7b7baadded696bf408e3189e3b32895e
Suchkov Denis <ny1828@yandex.ru>
2018-05-13 00:53:16 +0300

    init project

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		// config.foregroundFPS = 5;
+		// config.fullscreen = true;
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
